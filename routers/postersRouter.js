534f4d9b22cfbdbcfbfe04dbb40c1da0f657181e
Ilaria <maffiolailaria1@gmail.com>
2025-06-13 16:14:49 +0200

    [FEAT] Add store function to handle review submissions

--- routers/postersRouter.js
+++ routers/postersRouter.js
@@ -2,12 +2,15 @@ const express = require(`express`);
 const router = express.Router();
 const postersController = require(`../controllers/postersController`)
 
-const { index, show } = postersController;
+const { index, show, store } = postersController;
 
 // INDEX
 router.get('/', index);
 
 //SHOW
 router.get(`/:id`, show);
+
+// STORE
+router.post('/', store);
 
 module.exports = router;
