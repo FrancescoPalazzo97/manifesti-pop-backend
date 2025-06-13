b5880e6b131a4b5bccc6acc1636d074a8adace1f
FrancescoPalazzo97 <francescopalazzo97@gmail.com>
2025-06-13 14:56:59 +0200

    [FEAT] Add index and show controllers

--- controllers/postersController.js
+++ controllers/postersController.js
@@ -1,11 +1,11 @@
+const connection = require(`./data/db`)
+
 const index = (req, res) => {
 
     const sql = 'SELECT * FROM posters'
 
     connection.query(sql, (err, results) => {
-        if (err) {
-            return res.status(500).json({ error: 'Database query failed' })
-        }
+        if (err) return res.status(500).json({ error: `Database query failed: ${err}` });
 
         res.json(results);
     })
@@ -13,7 +13,18 @@ const index = (req, res) => {
 }
 
 const show = (req, res) => {
-    res.send(`Pagina di dettaglio del poster`)
+
+    const id = parseInt(req.params.id)
+    console.log(id)
+
+    const sql = `SELECT * FROM posters WHERE id = ?`
+
+    connection.query(sql, [id], (err, posterResult) => {
+        if (err) return res.status(500).json({ error: `Database query failed: ${err}` });
+        if (posterResult.length === 0 || posterResult[0].id === null) return res.status(404).json({ error: `Poster con ID ${id} non trovato.` });
+
+        res.json(posterResult)
+    })
 }
 
 module.exports = { index, show };
