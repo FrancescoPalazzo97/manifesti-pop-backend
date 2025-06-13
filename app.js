09c558e1eaf4c0ff7be366db9521b77e45ac70d0
FrancescoPalazzo97 <francescopalazzo97@gmail.com>
2025-06-13 15:00:23 +0200

    [FEAT] Add middlewares

--- app.js
+++ app.js
@@ -2,7 +2,13 @@ const express = require(`express`);
 const app = express();
 const port = process.env.DB_PORT || 3000;
 
-const postersRouter = require(`./routers/postersRouter`)
+const imagePath = require(`./middlewares/imagePath.js`);
+const notFound = require(`./middlewares/notFound.js`);
+const errorsHandler = require("./middlewares/errorsHandler.js");
+
+const postersRouter = require(`./routers/postersRouter`);
+
+app.use(imagePath);
 
 app.use(express.static(`public`));
 
@@ -13,6 +19,9 @@ app.use(`/posters`, postersRouter);
 app.get(`/`, (req, res) => {
     res.send(`Benvenuto`)
 })
+
+app.use(errorsHandler);
+app.use(notFound);
 
 app.listen(port, () => {
     console.log(`Il server è in ascolto`)
