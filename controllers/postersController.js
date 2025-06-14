b8cd47f89984fd7786d905523ffb826db1dc0276
FrancescoPalazzo97 <francescopalazzo97@gmail.com>
2025-06-14 16:18:35 +0200

    [UPD] Add storeOrders method with poster processing and order creation

--- controllers/postersController.js
+++ controllers/postersController.js
@@ -40,7 +40,7 @@ const show = (req, res) => {
     })
 }
 
-const storeReviews = (req, res, next) => {
+const storeReviews = (req, res) => {
     const id = parseInt(req.params.id);
 
     const { vote, text } = req.body;
@@ -59,4 +59,100 @@ const storeReviews = (req, res, next) => {
 
 }
 
-module.exports = { index, show, storeReviews };
+const storeOrders = (req, res) => {
+
+    // Recupero dati dallla body request
+    const {
+        name,
+        email,
+        address,
+        shipment_costs,
+        posters
+    } = req.body;
+
+    // Controllo che tutti i campi essenziali siano presenti
+    if (!name || !email || !address || !shipment_costs || !posters || posters.length === 0)
+        return res.status(400).json({ error: `Mancano dei dati obbligatori` });
+
+    // Creo la funzione che andrà a creare l'ordine una volta che avrò finito di elaborare i posters
+    const createOrder = () => {
+
+    }
+
+
+    // Avvio una transazione 
+    // Una transazione garantisce che tutte le operazione effettuate vadano a buon fine e nessuna modifica venga applicata in caso di errore
+    connection.beginTransaction(transError => {
+
+        if (transError)
+            return res.status(500).json({ error: `Errore: ${transError}` });
+
+        // Totale di tutti i prezzi senza spesa di spedizione
+        let subtotal = 0;
+
+        // Contatore che mi serve a contare i poster controllati
+        let postersChecked = 0;
+
+        // Array che mi va a contenere i dati elaborati di ogni poster => {poster_id, quantity, unit_price}
+        let elaboratedPosters = [];
+
+        // Funzioneper gestire gli errori
+        const handleError = (message) => {
+            // rollback annulla tutte le modifiche fatte fino ad ora all'interno di transaction
+            connection.rollback(() => {
+                res.status(500).json({ error: message });
+            })
+        }
+
+        // Mi ciclo i poster dell'ordine
+        posters.forEach((posterData, i) => {
+            // Mi passo id e quantità come valori numerici
+            const posterId = parseInt(posterData.id);
+            const quantity = parseInt(posterData.quantity)
+
+            // Recupero il poster corrispondente dal db
+            const sql = `
+            SELECT * FROM posters
+            WHERE id = ? AND available = 1
+            `
+            connection.query(sql, [posterId], (err, result) => {
+                if (err) return handleError(`Errore database: ${err}`);
+                if (result === 0) return handleError(`Poster ${id} non disponibile`);
+
+                const posterResult = result[0];
+
+                // Cointrollo se la quantità stock del poster recuperato dal db sia inferiore alla quantità dell'ordine
+                // Se inferiore mando messaggio di errore ed annullo tutto
+                if (posterResult.stock_quantity < quantity) return handleError(`Stock insufficente per ${posterResult.title}`);
+
+                // Imposto il prezzo al prezzo base
+                let price = posterResult.price
+
+                // Dopo di chè controllo se ci sono sconti
+                if (posterResult.discount > 0) {
+                    // Nel caso ci siano sconti vengono applicati
+                    price = posterResult.price * (1 - posterResult.discount / 100);
+                }
+
+                // Calcolo il subtotale
+                const posterSubtotal = price * quantity;
+
+                subtotal += posterSubtotal;
+
+                // Salvo il poster elaborato per usarlo nella creazione dell"ordine
+                elaboratedPosters.push({
+                    poster_id: posterResult.id,
+                    quantity: quantity,
+                    unit_price: price
+                })
+
+                postersChecked++;
+
+            })
+        });
+    })
+    // Invoco la funzione che mi crea l'ordine
+    createOrder();
+}
+
+module.exports = { index, show, storeReviews, storeOrders };
--- routers/postersRouter.js
+++ routers/postersRouter.js
@@ -2,7 +2,7 @@ const express = require(`express`);
 const router = express.Router();
 const postersController = require(`../controllers/postersController`)
 
-const { index, show, storeReviews } = postersController;
+const { index, show, storeReviews, storeOrders } = postersController;
 
 // INDEX
 router.get('/', index);
@@ -10,7 +10,10 @@ router.get('/', index);
 //SHOW
 router.get(`/:id`, show);
 
-// STORE
+// STORE REVIEWS
 router.post('/:id', storeReviews);
+
+// STORE ORDERS 
+router.post(`/order`, storeOrders)
 
 module.exports = router;
